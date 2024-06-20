23b564e39040fa4e9ffb4078bf2888bc5a97d361
Alex Su <alex801221@gmail.com>
2024-06-20 14:04:34 +0800

    vue-cli init

---
+++ src/main.js
@@ -0,0 +1,7 @@
+import { createApp } from 'vue'
+import { VanillaComponents } from '@flavorly/vanilla-components';
+import App from './App.vue'
+import router from './router'
+import store from './store'
+
+createApp(App).use(store).use(router).use(VanillaComponents).mount('#app')
