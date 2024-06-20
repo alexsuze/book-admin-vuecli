762e973d8d47681a0f0ec212b297008654d64060
Alex Su <alex801221@gmail.com>
2024-06-20 17:02:44 +0800

    登入頁面基本元件

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,5 @@
 <template>
-  <nav>
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
-  </nav>
-  <router-view/>
+  <router-view />
 </template>
 
 <style lang="scss">
@@ -12,19 +8,8 @@
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
-  color: #2c3e50;
+  color: hsl(210, 29%, 24%);
 }
 
-nav {
-  padding: 30px;
-
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-
-    &.router-link-exact-active {
-      color: #42b983;
-    }
-  }
-}
+@import '~bootstrap/scss/bootstrap'
 </style>
---
+++ src/views/LoginView.vue
@@ -0,0 +1,43 @@
+<script setup>
+
+</script>
+
+<template>
+    <div class="container-fluid login">
+        <div class="row">
+            <div class="col-9 login_bg_div"></div>
+            <div class="col-3 login_input_div">
+                <div>
+                    <h4>Readmoo 書目系統</h4>
+                </div>
+                <div>
+                    <label for="">帳號：</label>
+                    <input type="text">
+                </div>
+                <div>
+                    <label for="">密碼：</label>
+                    <input type="password">
+                </div>
+            </div>
+        </div>
+
+    </div>
+</template>
+
+<style lang="scss">
+.login {
+
+    /* 登入圖片區塊 */
+    .login_bg_div {
+        height: 100vh;
+        background-image: url('@/assets/images/login_img.jpeg');
+        background-size: cover;
+        background-repeat: no-repeat;
+    }
+
+    /* 登入操作區塊 */
+    .login_input_div {
+        border: 1px solid green;
+    }
+}
+</style>
