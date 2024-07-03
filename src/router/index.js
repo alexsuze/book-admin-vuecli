766d5e3627ea04e643fa40e3cba79a07c5f6e0f7
Alex Su <alex801221@gmail.com>
2024-07-03 17:07:44 +0800

    build test

--- src/router/index.js
+++ src/router/index.js
@@ -1,4 +1,4 @@
-import { createRouter, createWebHistory } from 'vue-router'
+import { createRouter, createWebHistory, createWebHashHistory } from 'vue-router'
 
 
 const routes = [
@@ -44,7 +44,8 @@ const routes = [
 ]
 
 const router = createRouter({
-  history: createWebHistory(process.env.BASE_URL),
+  history: createWebHashHistory(),
+  // history: createWebHistory(process.env.BASE_URL),
   routes
 })
 
--- vue.config.js
+++ vue.config.js
@@ -1,6 +1,8 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   publicPath: './',
+  outputDir: 'dist',
+  assetsDir: 'static',
   transpileDependencies: true,
   filenameHashing: false,
   configureWebpack: {
